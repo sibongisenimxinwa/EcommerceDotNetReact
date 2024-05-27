4c65678141de2e971735e555f63ec8d1a5aee0fa
Sibongiseni Mxinwa <sibongiseni.mxinwa@gmail.com>
2024-05-27 11:43:26 +0200

    End of section 5 : Error Handling

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,9 +5,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+   
+    public class ProductsController : BaseApiController
     {
         private readonly StoreContext _context;
         
@@ -20,13 +19,21 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return await _context.Products.ToListAsync();
+            var product = await _context.Products.ToListAsync();
+
+            if(product == null) return NotFound();
+
+            return product;
         }
 
         [HttpGet("{id}")] // api/products/3
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            var product = await _context.Products.FindAsync(id);
+
+            if(product == null) return NotFound();
+
+            return product;
         }
         
     }
